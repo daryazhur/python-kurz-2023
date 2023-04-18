2d5039938665fcebb0b8491eff1c2e78670c805d
Darya Zhurauliova <xczhurauliova@vutbr.cz>
2023-04-18 20:49:17 +0200

    DU2

---
+++ DU2_1.py
@@ -0,0 +1,49 @@
+"""
+Vyvíjíš software pro obchod s elektronickými součástkami. Firma eviduje informace o počtu součástek na skladě ve 
+slovníku. Klíč slovníku je kód součástky a hodnota klíče je počet součástek na skladě.
+Napiš software, který bude využívat prodavač v případě, že do obchodu přijde zákazník. Software se nejprve zeptá na 
+kód součástky a poté na množství, které si zákazník chce koupit. Obě informace si ulož. Následně naprogramuj 
+následující varianty:
+1) Pokud zadaný kód není ve slovníku, není součástka skladem. Vypiš tedy zprávu, že součástka není skladem.
+2) Pokud zadaná součástka na skladě je, ale je jí méně, než požaduje zákazník, vypiš text o tom, že lze prodat pouze 
+omezené množství kusů. Následně součástku odeber ze slovníku, protože je vyprodaná.
+3) Pokud zadaná součástka na skladě je a je jí dostatek, vypiš informaci, že poptávku lze uspokojit v plné výši, a 
+sniž počet součástek na skladě o množství požadované zákazníkem.
+"""
+
+sklad = {
+  "1N4148": 250,
+  "BAV21": 54,
+  "KC147": 147,
+  "2N7002": 97,
+  "BC547C": 10
+}
+
+kod = input('Zadejte kod soucastky: ')
+if kod in sklad:
+   print(f'Soucastka {kod} je skladem')
+else:
+   print('Soucastka neni skladem')
+   exit()
+mnozstvi = int(input('Zadejte mnozstvi: '))
+for hodnota in sklad.items():
+  if mnozstvi <= hodnota:
+    print('Soucastek je dostatek')
+  else:
+   print('Soucastka neni skladem')
+   exit()
+
+
+
+
+
+
+
+  
+
+    
+
+  
+
+    
+
